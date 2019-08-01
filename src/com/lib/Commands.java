1902e808e58e22dfd63341b2c182a56aaa0c27ef
Blake Olinger <blake.olinger@symbosoft.com>
2019-08-01 11:50:12 -0400

    -- added SW Daemon commands inner class

--- src/com/lib/Commands.java
+++ src/com/lib/Commands.java
@@ -4,4 +4,10 @@ public final class Commands {
     public static final String GUI_INIT = "00";
     public static final String PROGRAM_INIT = "01";
     public static final String SW_DAEMON_INIT = "011!";
+
+    public static class SWDaemon {
+        public static final String USER_ACTION_SENT = "001!";
+        public static final String CLOSE_SW_PART = "010!";
+        public static final String EXIT = "111!";
+    }
 }
