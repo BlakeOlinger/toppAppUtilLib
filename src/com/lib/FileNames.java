bd04dff1ca82d3eb1a52a01c045876ab3a8f6058
Blake Olinger <blake.olinger@symbosoft.com>
2019-08-01 13:48:52 -0400

    -- refactored FileNames and PathsList out of redundant occurrences in microservices

---
+++ src/com/lib/FileNames.java
@@ -0,0 +1,12 @@
+package com.lib;
+
+public final class FileNames {
+    public static final String INSTALL_DIRECTORY = "SolidWorks Daemon";
+    public static final String DB_DAEMON_JAR = "toppAppDBdaemon.jar";
+    public static final String GUI_INI = "GUI.ini";
+    public static final String GUI_CONFIG = "GUI.config";
+    public static final String SW_MS_CONFIG = "SWmicroservice.config";
+    public static final String SW_MS_EXE = "NuSWDaemon.exe";
+    public static final String DDTO = "DDTO.blemp";
+    public static final String PREVIEW_MS_CONFIG = "toppAppPreview.config";
+}
---
+++ src/com/lib/PathsList.java
@@ -0,0 +1,19 @@
+package com.lib;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public final class PathsList {
+    public static Path userIni = null;
+    public static Path toppAppConfig = null;
+    public static Path SWexe = null;
+    public static Path SWconfig = null;
+    public static Path DDTO = null;
+    public static ArrayList<Path> baseBlempPaths = new ArrayList<>();
+    public static Path userSelectedBlemp = null;
+    public static String blobString = null;
+    public static Path previewConfig = null;
+    public static Path DBdaemon = null;
+    public static Path installDirectory = null;
+    public static Path baseBlobDirectory = null;
+}
