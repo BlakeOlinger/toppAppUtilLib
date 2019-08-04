0caaf1e27f240bd3fdf4121582ca75dfd2c33169
Blake Olinger <blake.olinger@symbosoft.com>
2019-08-03 23:41:24 -0400

    -- EOD

---
+++ src/com/lib/InitializeCommands.java
@@ -0,0 +1,12 @@
+package com.lib;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InitializeCommands {
+    public static HashMap<String, String> commandMap = new HashMap<>(
+            Map.of(
+                    FileNames.DDTO, ""
+            )
+    );
+}
--- src/com/lib/InvalidInstallException.java
+++
@@ -1,7 +0,0 @@
-package com.lib;
-
-public final class InvalidInstallException extends Exception{
-    InvalidInstallException(String message) {
-        super(message);
-    }
-}
